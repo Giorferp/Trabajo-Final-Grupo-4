9f7dcbc7f48010a189cbf8f5735f0920ca6663de
Giovanni Fermin <giovannifermin8056@gmail.com>
2024-05-19 14:10:42 -0400

    Estudio del df y Visualización de los países
    Se realizó el un cambio de nombre a la base de datos a "df". Se describe el DataFrame para observar las variables a estudiar. Se crea una lista para visualizar los países por Continente. Una vez confirmados los países presentes en el DataFrame, se visualiza la información que posee cada uno

---
+++ Covid-19. Grupo 4.R
@@ -0,0 +1,31 @@
+#Llamado a tidyverse 
+
+library(tidyverse)
+
+#Cambio de nombre a la base de datos
+
+df <- Covid_G4_Base_de_Datos
+
+#Visializacion de variables en la Base de Datos 
+
+str(df)
+
+#Visualizacion de los paises que se tienen en el df
+
+Lista_Paises_Continente <- df %>% 
+  group_by(continent)%>% summarize (Paises= paste(unique(location),collapse = ","))
+
+#Seleccion de los paises a estudiar 
+#Ver informacion de United States, Russia, Germany, China,
+#Cuba, United Kingdom, Spain, Turkey, Iran and India
+
+df [df$location=="United States",]
+df [df$location== "Russia",]
+df [df$location== "Germany",]
+df [df$location== "China",]
+df [df$location== "Cuba",]
+df [df$location== "United Kingdom",]
+df [df$location== "Spain",]
+df [df$location== "Turkey",]
+df [df$location== "Iran",]
+df [df$location== "India",]
